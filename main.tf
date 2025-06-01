a2f32994bfc80c67e99429798351afedaee0bad5
jatin <malhotra.jatin21.com>
2025-06-01 13:21:42 +0530

    WIP: local changes to main.tf and tfvars

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "oci" {
   config_file_profile = "DEFAULT"
 }
 
-resource "oci_core_virtual_network" "example_vcn1" {
+resource "oci_core_vcn" "example_vcn1" {
   cidr_block     = "10.2.0.0/16"
   compartment_id = var.compartment_ocid
   display_name   = "example-vcn"
